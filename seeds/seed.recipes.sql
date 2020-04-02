e1c590fa1f9fb0191e812893ecc7e77c04a46e4f
Peter Rufo <peter@peterrufo.com>
2020-04-02 12:48:14 -0400

    update

--- seeds/seed.recipes.sql
+++ seeds/seed.recipes.sql
@@ -1,25 +1,20 @@
-INSERT INTO recipes (title, ingredients, instructions, source, photo_url)
+INSERT INTO recipes (title, ingredients, instructions, source)
 VALUES
 (
   'Chicken Piccata',
   '2 skinless and boneless chicken breasts, Sea salt, freshly ground black pepper, All-purpose flour, 6 tablespoons unsalted butter, 5 tablespoons extra-virgin olive oil, 1/3 cup fresh lemon juice, 1/4 cup brined capers, rinsed, 1/3 cup fresh parsley chopped',
   '1) Season chicken with salt and pepper. Dredge chicken in flour and shake off excess. 2 ) In a large skillet over medium high heat, melt 2 tablespoons of butter with 3 tablespoons olive oil. When butter and oil start to sizzle, add 2 pieces of chicken and cook for 3 minutes. When chicken is browned, flip and cook other side for 3 minutes. Remove and transfer to plate. Melt 2 more tablespoons butter and add another 2 tablespoons olive oil. When butter and oil start to sizzle, add the other 2 pieces of chicken and brown both sides in same manner. Remove pan from heat and add chicken to the plate. 3) Into the pan add the lemon juice, stock and capers. Return to stove and bring to boil, scraping up brown bits from the pan for extra flavor. Check for seasoning. Return all the chicken to the pan and simmer for 5 minutes. Remove chicken to platter. Add remaining 2 tablespoons butter to sauce and whisk vigorously. Pour sauce over chicken and garnish with parsley.',
-  'Giada De Laurentiis',
-  '''https://pinchofyum.com/wp-content/uploads/Chicken-Piccata.jpg'''
+  'Giada De Laurentiis'
 ),
 (
   'Guacamole',
   '4 ripe hass avocados, 3 tablespoons freshly squeezed lemon juice (1 lemon), 8 dashes hot pepper sauce, 1/2 cup small-diced red onion (1 small onion), 1 large garlic clove, minced, 1 teaspoon kosher salt, 1 teaspoon freshly ground black pepper, 1 medium tomato, seeded and small-diced',
   'Cut the avocados in half, remove the pits and scoop the flesh out of their shells into a large bowl. Immediately add the lemon juice, hot pepper sauce, onion, garlic, salt and pepper and toss well. Using a sharp knife, slice through the avocados in the bowl until they are finely diced. Add the tomato. Mix well and taste for salt and pepper. Serve with chips.',
-  'Ina Garten',
-  '''https://www.simplyrecipes.com/wp-content/uploads/2018/07/Guacamole-LEAD-1-600x840.jpg'''
-
+  'Ina Garten'
 ),
 (
   'Orecchiette Con Broccoli Di Rape and Sausage',
   '2 lbs. broccoli di rape, 1/2 lb. sweet Italian sausage, 5 tablespoons good olive oil, 3 large cloves garlic crushed, Salt, 1/4 teaspoon hot red pepper flakes, 1 to 3 tablespoons unsalted butter (optional), 1 cup or more excellent chicken stock, 6 quarts water, 1 lb. orecchiette pasta, 1 ounce or more Pecorino Romano cheese',
   '1) Preparing the Broccoli di Rape: Wash and spin-dry the broccoli di rape and remove the large tough leaves, leaving just tender leaves and flower buds. Cut off and discard the lower part of the stems, leaving broccoli about 8 inches long. Peel the stems by lifting strips from the stem end and drawing them up toward the bud area. 2) Preparing the Sauce: Peel the sausage, crumble it, and saute over moderately high heat in a tablespoon of olive oil. In a moment or two, add the crushed garlic and continue sauteing for another 2 to 3 minutes, until the sausage is lightly browned. Drain out the fat in the pan, and add a tablespoon of olive oil. Roughly line up the broccoli di rape and slice into 4-inch lengths: add to the pan, tossing. Taste, and season lightly with salt and pepper flakes. (At this point, you could start the pasta.) Cover the broccoli pan and let steam for several minutes: when the broccoli has wilted, stir in the butter, then the stock and bring to a boil. Taste again for seasoning, and let cook uncovered for several minutes more to reduce and concentrate the liquid. (At this point grate the cheese.) Again taste and correct seasoning. Set aside until the pasta is ready. 3) Boiling the Pasta: Heat 6 quarts of water in the stockpot, adding the salt, and timing so that the water is at a full boil by the time the broccoli di rape goes into the pan in the previous step. With the heat at its maximum, stir in the pasta and cover the pan until the water is at a boil. Then set the cover ajar and maintain at a moderate boil for 10 to 12 minutes, stirring and checking on the progress now and then. After 9 minutes, start testing by eating a piece. It should be just cooked through, but not quite as tender as you would like because it will cook a little more with the sauce. Drain at once, and proceed to the serving. 4) Serving the Pasta: Reheat the broccoli di rape as soon as the pasta has cooked and drained. Turn the hot pasta into the hot sauce and toss gently to blend. Taste carefully for seasoning, and remove from heat. Sprinkle on half of the freshly grated Romano cheese, and toss to blend. At once, turn the pasta onto the hot platter or plates, sprinkle with the rest of the Romano, and serve.',
-  'Lidia Bastianich',
-  '''https://gbc-cdn-public-media.azureedge.net/img74903.768x512.jpg'''
-
+  'Lidia Bastianich'
 )
